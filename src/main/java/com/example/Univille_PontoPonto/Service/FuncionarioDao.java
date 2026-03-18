0874c564a43fd81aad6c8e976648b3008e52a883
Lopesjoao13 <joao.lopes24@univille.br>
2026-03-18 12:54:49 -0300

    Atualização

---
+++ src/main/java/com/example/Univille_PontoPonto/Service/FuncionarioDao.java
@@ -0,0 +1,46 @@
+package com.example.Univille_PontoPonto.Service;
+
+import java.awt.image.RescaleOp;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FuncionarioDao extends BaseDAO{
+
+    public void exibirFuncionarios(){
+        var query = """
+                SELECT
+                    f.idFuncionario,
+                    f.matriculaFuncionario,
+                    f.nomeFuncionario,
+                    d.nomeDepartamento
+                FROM Funcionario f
+                JOIN Departamento d
+                    ON f.departamentoFuncionario = d.idDepartamento
+                ORDER BY f.nomeFuncionario;
+                """;
+
+        try (var con = con();
+        var pre = con.prepareStatement(query)){
+
+            ResultSet rs = pre.executeQuery();
+
+            System.out.println("\nLISTA DE FUNCIONÁRIOS");
+            System.out.println("------------------------------------------------------------");
+            System.out.printf("%-5s %-15s %-25s %-20s\n",
+                    "ID", "MATRÍCULA", "NOME", "DEPARTAMENTO");
+            System.out.println("------------------------------------------------------------");
+            while (rs.next()){
+                int id = rs.getInt("idFuncionario");
+                String matricula = rs.getString("matriculaFuncionario");
+                String nome = rs.getString("nomeFuncionario");
+                String departamento = rs.getString("nomeDepartamento");
+
+                System.out.printf("%-5d %-15s %-25s %-20s\n",
+                        id, matricula, nome, departamento);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/example/Univille_PontoPonto/UnivillePontoPontoApplication.java
+++ src/main/java/com/example/Univille_PontoPonto/UnivillePontoPontoApplication.java
@@ -1,5 +1,6 @@
 package com.example.Univille_PontoPonto;
 
+import com.example.Univille_PontoPonto.Service.FuncionarioDao;
 import com.example.Univille_PontoPonto.Service.RegistroDePontoDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +14,15 @@ public class UnivillePontoPontoApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(UnivillePontoPontoApplication.class, args);
 
-        RegistroDePontoDAO rp = new RegistroDePontoDAO(1, java.sql.Date.valueOf("2026-03-17"));
+		FuncionarioDao f = new FuncionarioDao();
+		f.exibirFuncionarios();
+
+        RegistroDePontoDAO rp = new RegistroDePontoDAO(1, java.sql.Date.valueOf("2026-03-10"));
         System.out.println(rp.buscarRegistrosDePonto());
+
+//		RegistroDePontoDAO rp = new RegistroDePontoDAO(1, java.sql.Date.valueOf("2026-03-17"));
+//		System.out.println(rp.buscarRegistrosDePonto());
+
 
 	}
 
